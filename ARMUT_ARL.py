b554329b99f6c87f91a708685a1db4c1a743044e
Eda Tasasız <101572852+Edatasasiz@users.noreply.github.com>
2023-09-27 13:59:55 +0300

    Update ARMUT_ARL.py

--- ARMUT_ARL.py
+++ ARMUT_ARL.py
@@ -1,28 +1,3 @@
-#########################
-# İş Problemi
-#########################
-import pandas as pd
-
-# Türkiye’nin en büyük online hizmet platformu olan Armut, hizmet verenler ile hizmet almak isteyenleri buluşturmaktadır.
-# Bilgisayarın veya akıllı telefonunun üzerinden birkaç dokunuşla temizlik, tadilat, nakliyat gibi hizmetlere kolayca
-# ulaşılmasını sağlamaktadır.
-# Hizmet alan kullanıcıları ve bu kullanıcıların almış oldukları servis ve kategorileri içeren veri setini kullanarak
-# Association Rule Learning ile ürün tavsiye sistemi oluşturulmak istenmektedir.
-
-
-#########################
-# Veri Seti
-#########################
-#Veri seti müşterilerin aldıkları servislerden ve bu servislerin kategorilerinden oluşmaktadır.
-# Alınan her hizmetin tarih ve saat bilgisini içermektedir.
-
-# UserId: Müşteri numarası
-# ServiceId: Her kategoriye ait anonimleştirilmiş servislerdir. (Örnek : Temizlik kategorisi altında koltuk yıkama servisi)
-# Bir ServiceId farklı kategoriler altında bulanabilir ve farklı kategoriler altında farklı servisleri ifade eder.
-# (Örnek: CategoryId’si 7 ServiceId’si 4 olan hizmet petek temizliği iken CategoryId’si 2 ServiceId’si 4 olan hizmet mobilya montaj)
-# CategoryId: Anonimleştirilmiş kategorilerdir. (Örnek : Temizlik, nakliyat, tadilat kategorisi)
-# CreateDate: Hizmetin satın alındığı tarih
-
 import pandas as pd
 pd.set_option('display.max_columns', None)
 # pd.set_option('display.max_rows', None)
@@ -54,6 +29,7 @@ df.info()
 # ServiceID ve CategoryID'yi "_" ile birleştirerek hizmetleri temsil edecek yeni bir değişken oluşturunuz.
 
 df["Hizmet"] = df["ServiceId"].astype(str) + "_" + df["CategoryId"].astype(str)
+
 
 # Adım 3:
 # Veri seti hizmetlerin alındığı tarih ve saatten oluşmaktadır, herhangi bir sepet tanımı (fatura vb. ) bulunmamaktadır.
